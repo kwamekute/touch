d0af78b74d82cb5d898e8ba4185e179d3e2cef30
Sampah Francis <francissampah2@gmail.com>
2021-09-16 18:03:29 +0000

    Removed Logout button from navbar

--- frontend/admin/src/components/DashboardNavbar.js
+++ frontend/admin/src/components/DashboardNavbar.js
@@ -1,41 +1,15 @@
-import { useState, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { Link as RouterLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import {
-  AppBar,
-  Badge,
-  Box,
-  Hidden,
-  IconButton,
-  Toolbar
-} from '@material-ui/core';
+import { AppBar, Box, Hidden, IconButton, Toolbar } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
-import NotificationsIcon from '@material-ui/icons/NotificationsOutlined';
-import InputIcon from '@material-ui/icons/Input';
-import { GlobalContext } from 'src/context/GlobalState';
 
 const DashboardNavbar = ({ onMobileNavOpen, ...rest }) => {
-  const [notifications] = useState([]);
-  const { logOutUser } = useContext(GlobalContext);
-
-  const navigate = useNavigate();
-
-  const handleSignout = () => {
-    logOutUser();
-    navigate('/login', { replace: true });
-  };
-
   return (
     <AppBar elevation={0} {...rest}>
       <Toolbar>
         <RouterLink to="/"></RouterLink>
         <Box sx={{ flexGrow: 1 }} />
-        <Hidden lgDown>
-          <IconButton onClick={handleSignout} color="inherit">
-            <InputIcon />
-          </IconButton>
-        </Hidden>
+
         <Hidden lgUp>
           <IconButton color="inherit" onClick={onMobileNavOpen}>
             <MenuIcon />
