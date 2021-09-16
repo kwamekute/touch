cd7113782998946765a2f28f941b04c500e974e2
Sampah Francis <francissampah2@gmail.com>
2021-09-16 17:44:02 +0000

    Added loading animation on Data fetch

--- frontend/admin/src/components/LoadingBackdrop.js
+++ frontend/admin/src/components/LoadingBackdrop.js
@@ -1,19 +1,13 @@
 import { Backdrop, CircularProgress } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-  backdrop: {
-    zIndex: theme.zIndex.drawer + 1,
-    color: '#fff'
-  }
-}));
 
 function LoadingBackdrop({ open }) {
-  const classes = useStyles();
   console.log('I rendered=>', open);
   return (
     <div>
-      <Backdrop className={classes.backdrop} open={open}>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+      >
         <CircularProgress color="inherit" />
       </Backdrop>
     </div>
