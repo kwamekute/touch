a282c39eaa7422e18822bf1db610accc4e368327
Sampah Francis <francissampah2@gmail.com>
2021-09-18 02:33:54 +0000

    Added Percentage Statistics

--- frontend/admin/src/routes/ProtectedRoute.js
+++ frontend/admin/src/routes/ProtectedRoute.js
@@ -1,20 +1,12 @@
 import { useContext, useEffect } from 'react';
-import { Navigate, useLocation, useNavigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { GlobalContext } from '../context/GlobalState';
 
 export function ProtectedRoute({ children }) {
-  const { auth, getBookings, getStats, user, bookings, error } =
+  const { auth, getBookings, getStats, user, bookings } =
     useContext(GlobalContext);
-  const navigate = useNavigate();
   useEffect(() => {
-    getBookings(user).then(() => {
-      if (
-        error === 'Access not authorized, There was an error => jwt expired'
-      ) {
-        logOutUser();
-        navigate('/login', { replace: true });
-      }
-    });
+    getBookings(user);
     //eslint-diable-next-line react-hooks/exhustive-deps;
   }, []);
 
