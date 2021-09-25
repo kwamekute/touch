f199163d5d3d7b8e1e8ba4c0d7ad065f1203f0ec
Sampah Francis <francissampah2@gmail.com>
2021-09-25 08:57:39 +0000

    Added Auth interceptor

--- frontend/admin/src/components/Logo.js
+++ frontend/admin/src/components/Logo.js
@@ -1,5 +1,5 @@
 const Logo = (props) => (
-  <img width="18%" alt="Logo" src="/static/lux-touch-logo.png" {...props} />
+  <img width="16%" alt="Logo" src="/static/lux-touch-logo.png" {...props} />
 );
 
 export default Logo;
